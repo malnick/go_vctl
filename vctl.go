4ee0fb86c557df9ad0c9d8d3ee470fc4e5802eb5
malnick <malnick@gmail..com>
2015-07-30 11:29:59 -0700

    maping arbitrary puppet keys to the versions struct

--- vctl.go
+++ vctl.go
@@ -9,37 +9,40 @@ import (
 )
 
 // Request for Puppet Versions: "http://puppet.ec2.srcclr.com:1015/versions"
-type PuppetVersions struct {
-	AnalyticsVersionProduction       string `json:"analytics_version_production"`
-	AnalyticsVersionQa               string `json:"analytics_version_qa"`
-	BlogVersionProduction            string `json:"blog_version_production"`
-	FrontendStaticVersionDev         string `json:"frontend_static_version_dev"`
-	FrontendStaticVersionProduction  string `json:"frontend_static_version_production"`
-	FrontendStaticVersionQa          string `json:"frontend_static_version_qa"`
-	LibrarianVersionProduction       string `json:"librarian_version_production"`
-	LibrarianVersionQa               string `json:"librarian_version_qa"`
-	NotificationsVersionProduction   string `json:"notifications_version_production"`
-	NotificationsVersionQa           string `json:"notifications_version_qa"`
-	PlatformVersionProduction        string `json:"platform_version_production"`
-	PlatformVersionQa                string `json:"platform_version_qa"`
-	ScmAgentVersionProduction        string `json:"scm_agent_version_production"`
-	ScmAgentVersionQa                string `json:"scm_agent_version_qa"`
-	SearchVersionProduction          string `json:"search_version_production"`
-	SearchVersionQa                  string `json:"search_version_qa"`
-	VulnerabilitiesVersionProduction string `json:"vulnerabilities_version_production"`
-	VulnerabilitiesVersionQa         string `json:"vulnerabilities_version_qa"`
-	WebhooksVersionProduction        string `json:"webhooks_version_production"`
-	WebhooksVersionQa                string `json:"webhooks_version_qa"`
-}
+//type PuppetVersions struct {
+//	ServiceName string
+//	AnalyticsVersionProduction       string `json:"analytics_version_production"`
+//	AnalyticsVersionQa               string `json:"analytics_version_qa"`
+//	BlogVersionProduction            string `json:"blog_version_production"`
+//	FrontendStaticVersionDev         string `json:"frontend_static_version_dev"`
+//	FrontendStaticVersionProduction  string `json:"frontend_static_version_production"`
+//	FrontendStaticVersionQa          string `json:"frontend_static_version_qa"`
+//	LibrarianVersionProduction       string `json:"librarian_version_production"`
+//	LibrarianVersionQa               string `json:"librarian_version_qa"`
+//	NotificationsVersionProduction   string `json:"notifications_version_production"`
+//	NotificationsVersionQa           string `json:"notifications_version_qa"`
+//	PlatformVersionProduction        string `json:"platform_version_production"`
+//	PlatformVersionQa                string `json:"platform_version_qa"`
+//	ScmAgentVersionProduction        string `json:"scm_agent_version_production"`
+//	ScmAgentVersionQa                string `json:"scm_agent_version_qa"`
+//	SearchVersionProduction          string `json:"search_version_production"`
+//	SearchVersionQa                  string `json:"search_version_qa"`
+//	VulnerabilitiesVersionProduction string `json:"vulnerabilities_version_production"`
+//	VulnerabilitiesVersionQa         string `json:"vulnerabilities_version_qa"`
+//	WebhooksVersionProduction        string `json:"webhooks_version_production"`
+//	WebhooksVersionQa                string `json:"webhooks_version_qa"`
+//}
+
+type PuppetVersions map[string]interface{}
 
 // Our bare page
 type Page struct {
 	Title string
 	Body  []byte
-	Pv    *PuppetVersions
+	Pv    PuppetVersions
 }
 
-func puppetversions(url string) (*PuppetVersions, error) {
+func puppetversions(url string) (PuppetVersions, error) {
 	// Get response and handle any errors on return
 	resp, err := http.Get(url)
 	defer resp.Body.Close()
@@ -61,7 +64,7 @@ func puppetversions(url string) (*PuppetVersions, error) {
 		return nil, err
 	}
 
-	return &v, nil
+	return v, nil
 }
 
 func loadPage(title string) (*Page, error) {
@@ -76,7 +79,8 @@ func loadPage(title string) (*Page, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Println("WebHooks Version QA: ", string(pv.WebhooksVersionQa))
+	log.Println("VERSIONS\n")
+	log.Println("%v", pv["scm_agent_version_production"])
 
 	return &Page{
 			Title: title,
