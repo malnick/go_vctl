19aeb11b07304b98b84dd99f130f460d6686dbcb
malnick <malnick@gmail..com>
2015-07-30 06:37:01 -0700

    vctl.go

--- vctl.go
+++ vctl.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"html"
+	"io/ioutil"
 	"log"
-	"net/http"
-	// My library
-	"github.com/malnick/vctl_lib"
 )
 
-func config() {
-
-	config := GetConfig()
-	log.Printf(config.Pvendpoint)
+type Page struct {
+	Title string
+	Body  []byte
 }
 
-func run() {
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+func main() {
+	p1 := &Page{Title: "VersionCtl", Body: []byte("Eventually a go template...")}
+	p1.save()
+	p2, _ := loadPage("VerstionCtl")
+	log.Printf(string(p2.Body))
 }
