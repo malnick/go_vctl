2fa01f1330f97cfd751a002a3ad8c3ebe53f650b
malnick <malnick@gmail..com>
2015-08-02 08:47:04 -0700

    v

--- vctl.go
+++ vctl.go
@@ -8,13 +8,18 @@ import (
 	"net/http"
 )
 
+// A map for Puppet Versions JSON
 type PuppetVersions map[string]interface{}
+
+// A map for Production Versions JSON
+type ProductionVersions map[string]interface{}
 
 // Our bare page
 type Page struct {
 	Title string
 	Body  []byte
 	Pv    PuppetVersions
+	Prodv ProductionVersions
 }
 
 func puppetversions(url string) (PuppetVersions, error) {
@@ -42,11 +47,45 @@ func puppetversions(url string) (PuppetVersions, error) {
 	return v, nil
 }
 
+func productionversions(url string) (ProductionVersions, error) {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	jsonDataFromHttp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	//available_services := make(map[string]interface{})
+	var v ProductionVersions
+	err = json.Unmarshal(jsonDataFromHttp, &v)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("AVAILABLE SERVICES: ", v)
+
+	return v, nil
+}
+
 func loadPage(title string) (*Page, error) {
+	// Get the versions from the puppet master
+	log.Println("Getting Puppet Versions - Make sure VPN is on!")
 	pv, err := puppetversions("http://puppet.ec2.srcclr.com:1015/versions")
 	if err != nil {
 		log.Println("Failed to get Puppet Versions from http://puppet.ec2.srcclr.com:1015/versions\n")
 		log.Println(err)
+	}
+
+	// Get running versions
+	log.Println("Getting available services...")
+	prv, err := productionversions("http://localhost:3000/services")
+	if err != nil {
+		log.Println("Failed getting production versions")
 	}
 
 	filename := title + ".html"
@@ -61,11 +100,13 @@ func loadPage(title string) (*Page, error) {
 			Title: title,
 			Body:  body,
 			Pv:    pv,
+			Prodv: prv,
 		},
 		nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Loading page view...")
 	title := "versionctl"
 	p, err := loadPage(title)
 	if err != nil {
@@ -73,12 +114,14 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Parse the template, execute and write it to stdout for good measure
+	log.Println("Parsing go template...")
 	t, _ := template.ParseFiles("versionctl.html")
 	t.Execute(w, p)
 	log.Println("Serving:\n", string(p.Title), string(p.Body))
 }
 
 func main() {
+	log.Println("Starting vctl...")
 	http.HandleFunc("/", viewHandler)
 	http.ListenAndServe(":9000", nil)
 }
