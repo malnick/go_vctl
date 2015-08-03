a2ff7bdf8e8da455db5e4679f25a469c52914466
malnick <malnick@gmail..com>
2015-08-03 12:24:06 -0700

    vctl.go

--- vctl.go
+++ vctl.go
@@ -71,16 +71,6 @@ func getServices(url string) (interface{}, error) {
 	return available_services, nil
 }
 
-//func parseInfo(info map[string]string) (version string, err error) {
-//	for k, v := range info {
-//		for key, value := range info {
-//			version := info["version"]
-//			return version, nil
-//		}
-//	}
-//	return "Didn't parse the info map", err
-//}
-
 func queryServiceVersion(endpoint string) (version string, err error) {
 	log.Println("Querying SERVICE address: ", endpoint)
 	query_arry := []string{"http://", endpoint, "/info"}
@@ -162,57 +152,6 @@ func getVersions(services interface{}) (runningversions map[string]map[string]st
 	runningversions = rv
 	return runningversions, nil
 }
-
-// If our string has one component it's a service address
-//				if len(query_arry) == 1 {
-//					mgmt_ip := query_arry[0]
-//
-//					// Add info endpoint
-//					info_uri_slice := []string{mgmt_ip, "/info"}
-//					info_uri := strings.Join(info_uri_slice, "")
-//
-//					version, _ := parseInfo(info_response)
-//
-//					runningversions.Service[service_name][info_uri] = append(runningversions.Service[service_name][info_uri], version)
-
-// If our string has two components then use the mgmt address
-//				} else if len(query_arry) == 2 {
-//					mgmt_ip := query_arry[1]
-//
-//					// Add info endpoint
-//					info_uri_slice := []string{mgmt_ip, "/info"}
-//					info_uri := strings.Join(info_uri_slice, "")
-//					log.Println("Querying SERVICE address for ", service_name, ": ", mgmt_ip)
-//					// Query the URI
-//					resp, err := http.Get(info_uri)
-//					defer resp.Body.Close()
-//					if err != nil {
-//						log.Println("ERROR querying ", service_name, " ", err)
-//						return nil, err
-//					}
-//					// Get data and unmarshel the JSON to our map
-//					jsonDataFromHttp, err := ioutil.ReadAll(resp.Body)
-//					if err != nil {
-//						log.Println("ERROR unmarsheling data for ", service_name, " from ", jsonDataFromHttp)
-//						return nil, err
-//					}
-//					var info_response map[string]interface{}
-//					err = json.Unmarshal(jsonDataFromHttp, &info_response)
-//					if err != nil {
-//						return nil, err
-//					}
-//					// Parse out the version from the response
-//					log.Println("INFO for ", service_name, ":\n", info_response)
-//					versions := parseInfo(service_name, info_response)
-//
-//					return versions, nil
-//
-//					// If our string as more than two components, error
-//				} else if len(query_arry) > 2 {
-//					return nil, err
-//				}
-//	return runningversions, err
-//}
 
 func loadPage(title string) (*Page, error) {
 	// Get the versions from the puppet master
